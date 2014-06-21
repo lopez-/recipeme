e835eaf09d890c061295a846a42f72bae7239ae6
lopez- <victorlopezse@gmail.com>
2014-06-21 20:11:35 +0100

    bfrimg

--- Gemfile
+++ Gemfile
@@ -38,4 +38,6 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
 
-gem 'paperclip'
+gem 'carrierwave'
+
+gem 'bootstrap-sass', '~> 2.3.2'
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -45,6 +45,6 @@ class RecipesController < ApplicationController
 	private
 
 		def recipe_params
-			params.require(:recipe).permit(:title, :ingredients, :preparation)
+			params.require(:recipe).permit(:title, :ingredients, :preparation, :image)
 		end
 end
